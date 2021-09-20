7ee71511e21dfdf43d168bb034bedfb6ddede5c6
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-20 14:19:55 +0100

    Implement Hamburger Menu for navigation

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
 const Navbar = () => {
   const links = [
@@ -15,12 +17,36 @@ const Navbar = () => {
     },
   ];
 
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
   return (
     <nav className="navBar">
-      <ul>
+      <button type="button" onClick={handleToggle}>
+        {navbarOpen ? (
+          <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} />
+        ) : (
+          <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+        )}
+      </button>
+      <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
         {links.map((link) => (
           <li key={link.id}>
-            <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
+            <NavLink
+              to={link.path}
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              {link.text}
+            </NavLink>
           </li>
         ))}
       </ul>
