f2b8ed459ca58335e5332371cdbf80cbe66b8567
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-20 14:48:39 +0100

    Deploy app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import TodoContainer from './functionBased/components/TodoContainer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
